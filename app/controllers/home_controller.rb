9598fc2dce2db8b73690422c843e448b351a91a8
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-05-01 15:29:58 -0700

    uses javascript to get new images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,4 @@
 class HomeController < ApplicationController
   def index
-    access_token = "233483179.f9c78cf.7e493e9b2fba4f5c8062c88d2da896b0"
-    client = Instagram.client(access_token: access_token)
-    default_search = client.tag_search('foodporn')
-
-    @results = client.tag_recent_media(default_search.first.name)
   end
 end
--- config/initializers/instagram.rb
+++
@@ -1,6 +0,0 @@
-require "instagram"
-
-Instagram.configure do |config|
-  config.client_id = ENV["INSTAGRAM_CLIENT"]
-  config.access_token = ENV["ACCESS_TOKEN"]
-end
