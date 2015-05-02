a053e66f2cf8d4dd2565df2b68851bc678b77d89
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-05-01 17:40:01 -0700

    images displayed on the footer

--- app/assets/javascripts/background_changer.js
+++ app/assets/javascripts/background_changer.js
@@ -1,27 +1,27 @@
-$(document).ready(function(){
-  var urlArray = []
-  var feed = new Instafeed({
-      get: 'tagged',
-      tagName: 'foodporn',
-      clientId: 'f9c78cfd275943e1aa93165e83ee13e3',
-      limit: 1,
-      success: function(image){
-        url = image.data[0].images.low_resolution.url;
+// $(document).ready(function(){
+//   var urlArray = []
+  // var feed = new Instafeed({
+  //     get: 'tagged',
+  //     tagName: 'foodporn',
+  //     clientId: 'f9c78cfd275943e1aa93165e83ee13e3',
+  //     limit: 1,
+  //     success: function(image){
+  //       url = image.data[0].images.low_resolution.url;
         
-        //only post unique images
-        if( urlArray.indexOf(url) === -1 ){
-          urlArray.push(url);
-          $('#lol').prepend('<img src="'+ url + '"/>')
-        }
-      },
-      mock: function(){
-        return true
-      }
-  });
+  //       //only post unique images
+  //       if( urlArray.indexOf(url) === -1 ){
+  //         urlArray.push(url);
+  //         $('#lol').prepend('<img src="'+ url + '"/>')
+  //       }
+  //     },
+  //     mock: function(){
+  //       return true
+  //     }
+  // });
 
-  setInterval(function(){
-    feed.run()
-  }, 200);
+//   setInterval(function(){
+//     feed.run()
+//   }, 200);
   
     
-});
+// });
--- app/assets/javascripts/components/index_page.js.jsx
+++ app/assets/javascripts/components/index_page.js.jsx
@@ -1,7 +1,35 @@
 var IndexPage = React.createClass({
+  getInitialState: function() {
+    return{urlArray: []}
+  },
+
+  componentWillMount: function(){
+    var self = this;
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'foodporn',
+        clientId: 'f9c78cfd275943e1aa93165e83ee13e3',
+        limit: 30,
+        success: function(images){
+          console.log(images.data)
+          url = images.data;
+          self.setState({urlArray: url});
+        },
+        mock: function(){
+          return true
+        }
+    });
+
+    feed.run();
+  },
 
 
   render: function() {
+    urlArrayElements = [];
+    this.state.urlArray.forEach(function(url) {
+      urlArrayElements.push(<img src={url.images.low_resolution.url} /> )
+    });
+
     return(
       <div>
         <div className="search_bar">
@@ -9,8 +37,7 @@ var IndexPage = React.createClass({
         </div>
 
         <div className="footer">
-
-        
+          {urlArrayElements}
         </div>
       </div>
     );
