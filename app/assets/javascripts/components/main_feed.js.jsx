c623a37d89df5a394255b6abed5b9a869017588a
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-05-03 00:29:23 -0700

    added search bar

---
+++ app/assets/javascripts/components/main_feed.js.jsx
@@ -0,0 +1,43 @@
+var MainFeed = React.createClass({
+  getInitialState: function(){
+    return {urlArray: [], tag: ""}
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    console.log(nextProps.tag);
+    this.setState({tag: nextProps.tag})
+  },
+
+  render: function(){
+    var self = this
+  
+    var feed = new Instafeed({
+      get: 'tagged',
+      tagName: self.state.tag,
+      clientId: 'f9c78cfd275943e1aa93165e83ee13e3',
+      limit: 8,
+      success: function(images){
+        url = images.data;
+        self.setState({urlArray: url});
+      },
+      mock: function(){
+        return true
+      }
+    });
+  
+    setInterval(function() {
+      feed.run();
+    }, 5000)
+
+    urlArrayElements = [];
+    this.state.urlArray.forEach(function(url) {
+      urlArrayElements.push(<img src={url.images.low_resolution.url} /> )
+    });
+
+    return(
+      <div>
+        {urlArrayElements}
+      </div>
+    );
+  }
+})
