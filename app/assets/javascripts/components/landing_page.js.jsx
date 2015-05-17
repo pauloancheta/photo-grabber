52e8c88c0ac9e01cebc942188d5e5eccc92c61cb
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-05-17 14:59:00 -0700

    added styling to landing page

--- app/assets/javascripts/components/landing_page.js.jsx
+++ app/assets/javascripts/components/landing_page.js.jsx
@@ -1,9 +1,12 @@
 var LandingPage = React.createClass({
   render: function(){
     return(
-      <div>
-        <h1>Welcome to PhotoGrabber</h1>
-        <p>Free instagram search of pictures by tags.</p>
+      <div className="landing_page">
+        <div className="landing_page__description">
+          <h1 className="free">FREE</h1>
+          <h1>live instagram feed</h1>
+          <p>Search for your favourite hashtag!</p>
+        </div>
       </div>
     )
   }
